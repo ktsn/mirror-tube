0f2fe8a724c9291184ebeb44212899a2465da7ff
katashin <ktsn55@gmail.com>
2014-12-07 23:31:11 +0900

    show the current playback rate as text.

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,21 @@
 var Popup = {
   initialize: function(form) {
+    this.form = form;
+
     VideoCtrl.isMirrored(function(mirrored) {
       form.mirror.checked = mirrored;
     });
     VideoCtrl.getPlaybackRate(function(rate) {
       form.playbackRate.value = rate;
+      form.playbackRatePreview.innerHTML = rate.toFixed(1);
     });
 
     form.mirror.addEventListener('change', function(e) {
       VideoCtrl.changeMirrorMode(e.target.checked);
     });
-    form.playbackRate.addEventListener('change', function(e) {
+    form.playbackRate.addEventListener('input', function(e) {
       VideoCtrl.changePlaybackRate(e.target.value);
+      form.playbackRatePreview.innerHTML = parseFloat(e.target.value).toFixed(1);
     });
   }
 };
@@ -20,7 +24,8 @@ window.onload = function() {
   var formEl = document.getElementById('video-controller');
   var form = {
     mirror: formEl.mirror,
-    playbackRate: formEl.rate
+    playbackRate: formEl.rate,
+    playbackRatePreview: document.getElementById('playback-rate-preview')
   };
 
   VideoCtrl.initVideo(function() {
